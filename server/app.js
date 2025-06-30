5b86bc89884664c4ada1fab4da27c9d1b2ddf48b
kofi arhin <kofiarhin69@gmail.com>
2025-06-30 10:33:58 +0100

    fixed default route issues

--- server/app.js
+++ server/app.js
@@ -6,6 +6,11 @@ const app = express();
 // setup middlewares
 app.use(cors());
 
+// routes
+app.get("/", (req, res) => {
+  return res.json({ message: "welcome to newsletter" });
+});
+
 app.get("/api/auth", (req, res) => {
   return res.json({ message: "hello world" });
 });

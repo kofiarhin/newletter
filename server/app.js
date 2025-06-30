367dca220bf3da2d7e16ce69b4cea8e03039242c
kofi arhin <kofiarhin69@gmail.com>
2025-06-30 10:30:25 +0100

    fixed cors issues

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+// setup middlewares
+app.use(cors());
 
 app.get("/api/auth", (req, res) => {
   return res.json({ message: "hello world" });

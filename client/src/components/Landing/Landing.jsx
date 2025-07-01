b60e8d70efc48b07d098355c276eb9780a5c8bed
kofi arhin <kofiarhin69@gmail.com>
2025-07-01 14:40:56 +0100

    ui clean up

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,10 +1,14 @@
-
 const Landing = () => {
   return (
     <div>
-        <h1>Landing Page</h1>
+      <h1>Landing Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+        debitis illo deserunt esse optio impedit aperiam obcaecati labore,
+        voluptatum repudiandae.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;

89f71a09dacfad21b4bef54d10279118f13b8e52
kofi arhin <kofiarhin69@gmail.com>
2025-07-02 11:54:37 +0100

    update

--- server/controllers/subscriberController.js
+++ server/controllers/subscriberController.js
@@ -1,10 +1,38 @@
+const User = require("../models/userModel");
+const sendEmail = require("../utility/sendEmail");
+
+// get subscribers
 const getSubscribers = async (req, res) => {
   return res.json({ message: "get subscribers" });
 };
 
 const createSubscribers = async (req, res) => {
-  const { email, name } = req.body;
-  return res.json({ name, email });
+  try {
+    const { email, name } = req.body;
+    if (!name || !email) {
+      res.status(400);
+      throw new Error("please fill out all fields");
+    }
+
+    // create user
+    await User.deleteMany();
+    const user = await User.create({ name, email });
+    console.log({ user });
+
+    // send email
+    const emailOptions = {
+      to: email,
+      subject: "Welcome and confirm email",
+      text: "some text to confirm email",
+    };
+
+    await sendEmail(emailOptions);
+
+    return res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {

05471442f555c4f67982a365d782e97a81f28d6f
kofi arhin <kofiarhin69@gmail.com>
2025-07-02 15:15:39 +0100

    update ui clean up: verififed.html

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const emailRoutes = require("./routes/emailRoutes");
 const subscriberRoutes = require("./routes/subscriberRoutes");
+const path = require("path");
 
 const app = express();
 
+const staticPath = path.join(__dirname, "public");
 // setup middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(staticPath));
 
 // routes
 app.get("/", (req, res) => {
--- server/constants/constants.js
+++ server/constants/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://newletter-g95o.onrender.com/";
+const BASE_URL = "https://newletter-g95o.onrender.com";
 
 module.exports = {
   BASE_URL,
